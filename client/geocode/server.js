b38a86be68830f7fe650f1d1a4d91390b743bfac
Sam Goto <goto@goto-macbookair2.roam.corp.google.com>
2018-01-30 16:36:20 -0800

    somewhat working version

--- client/geocode/server.js
+++ client/geocode/server.js
@@ -8,15 +8,7 @@ app.get("/", function(req, res) {
       <head>
         <style>
            body {
-            perspective: 800px;
-           }
-           .arrow {
-            width: 200px;
-            height: 200px;
-            margin: 100px;
-            color: white;
-            background-image: url(https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png);
-            background-size: contain;
+            perspective: 200px;
            }
 
            .cube {
@@ -25,31 +17,34 @@ app.get("/", function(req, res) {
              left: 50%;
              text-align: center;
              font-size: 4em;
-             width: 2em;
+             width: 16em;
              margin: 1.5em auto;
              transform-style: preserve-3d;
-             transform: rotateX(90deg) rotateY(10deg);
+             /* transform: rotateX(90deg); */
+             transform: rotateX(0deg) translateY(-4em) translateX(-8em) rotateY(0deg)
            }
 
            .side {
              position: absolute;
-             width: 2em;
-             height: 2em;
+             width: 4em;
+             height: 4em;
   
+            /*
              background: rgba(255, 99, 71, .6);
-             border: 1px solid rgba(0, 0, 0, .5);
+            */
+             border: 1px dashed rgba(0, 0, 0, .5);
   
              color: white; 
              text-align: center;
              line-height: 2em;
            }
 
-          .front  { transform:                  translateZ(1em); }
-          .top    { transform: rotateX( 90deg)  translateZ(1em); }
-          .right  { transform: rotateY( 90deg)  translateZ(1em); }
-          .left   { transform: rotateY(-90deg)  translateZ(1em); }
-          .bottom { transform: rotateX(-90deg)  translateZ(1em); }
-          .back   { transform: rotateY(-180deg) translateZ(1em); }
+           .front  { transform:                  translateZ(2em); width: 16em; }
+           .top    { transform: rotateX( 90deg)  translateZ(2em); width: 16em; height: 4em; }
+           .right  { transform: rotateY( 90deg)  translateZ(14em); width: 4em; }
+           .left   { transform: rotateY(-90deg)  translateZ(2em); width: 4em; }
+           .bottom { transform: rotateX(-90deg)  translateZ(2em); width: 16em; height: 4em; }
+           .back   { transform: rotateY(-180deg) translateZ(2em); width: 16em; }
 
           .map {
              transform: rotateZ(90deg) rotateY(-70deg) translate(-50%);
@@ -58,10 +53,12 @@ app.get("/", function(req, res) {
              top: -15%;
           }
 
-            .back, .bottom {
+          .back, .bottom {
+             background-size: cover;
+             background-repeat: no-repeat;
+             background-position: center center; 
              opacity: 0.6;
              filter: alpha(opacity=60);
-             background-size: contain;
           }
 
         </style>
@@ -69,68 +66,88 @@ app.get("/", function(req, res) {
       <body>
 
            <div id="arrow" class="cube">
-             <div class="side  front">1</div>
-             <div class="side   back" id="back">6</div>
-             <div class="side  right">4</div>
-             <div class="side   left">3</div>
-             <div class="side    top">5</div>
-             <div class="side bottom" id="bottom">2</div>
+             <div class="side  front"></div>
+             <div class="side   back" id="back"></div>
+             <div class="side  right" id="right"></div>
+             <div class="side   left" id="left"></div>
+             <div class="side    top"></div>
+             <div class="side bottom" id="bottom"></div>
            </div>
+
+           <br><textarea id="debug" rows="2" cols="120"></textarea>
 
         <!--
 
 
         <div id="arrow2" class="arrow"></div>
 
-        <br><textarea id="debug" rows="2" cols="120"></textarea>
         -->
 
         <script>
 
-           function debug(str) { document.getElementById("debug").value = str; }
+           function debug(str) {
+             document.getElementById("debug").value = "[" +  new Date() + "] " + str;
+           }
 
-            let lat = 37.4183027;
-            let lon = -122.0863113;
+           function background(user, flag) {
+             // document.getElementById("map").src = 
+             let back =
+             "https://maps.googleapis.com/maps/api/streetview?" +
+             "size=400x400" +
+             "&location=" + user.lat + "," + user.lon +
+             "&fov=90&pitch=10" +
+             "&key=AIzaSyBvQ4O0ju12WpYVKO-SY82-ntAicgXLf8o";
+             // console.log(back);
 
-            let flag = {lat: 37.4183027, lon: -122.0864113};
-            
-            // document.getElementById("map").src = 
-            let back =
-            "https://maps.googleapis.com/maps/api/streetview?" +
-            "size=400x400" +
-            "&location=" + lat + "," + lon +
-            "&fov=90&heading=235&pitch=10" +
-            "&key=AIzaSyBvQ4O0ju12WpYVKO-SY82-ntAicgXLf8o";
+             // document.getElementById("back").style.backgroundImage =
+             //  "url('" + back + "&heading=" + user.heading + "')";
+             // document.getElementById("left").style.backgroundImage = 
+             //   "url('" + back + "&heading=" + (heading - 90) + "')";
+             // document.getElementById("right").style.backgroundImage = 
+             //   "url('" + back + "&heading=" + (heading + 90) + "')";
 
-            document.getElementById("back").style.backgroundImage = "url('" + back + "')";
+             let bottom = "https://maps.googleapis.com/maps/api/staticmap?" +
+             "&zoom=19&size=600x600&sensor=false" +
+              // "&maptype=satellite" +
+             "&center=" + user.lat + "," + user.lon +
+             "&markers=color:red%7Clabel:A%7C" + user.lat + "," + user.lon +
+             "&markers=color:blue%7Clabel:B%7C" + flag.lat + "," + flag.lon;
+              document.getElementById("bottom").style.backgroundImage = "url('" + bottom + "')";
+           }
 
-            let bottom = "https://maps.googleapis.com/maps/api/staticmap?" +
-            "&zoom=20&size=600x600&sensor=false&maptype=satellite" +
-            "&center=" + lat + "," + lon +
-            "&markers=color:red%7Clabel:A%7C" + lat + "," + lon +
-            "&markers=color:blue%7Clabel:B%7C" + flag.lat + "," + flag.lon;
+           let user = {lat: 37.4183027, lon: -122.0863113, heading: 235};
+           let flag = {lat: 37.4183027, lon: -122.0864113};
+           background(user, flag);
 
-            document.getElementById("bottom").style.backgroundImage = "url('" + bottom + "')";
-
-             navigator.geolocation.getCurrentPosition(function(position) {
-               return;
+           navigator.geolocation.getCurrentPosition(function(position) {
+               //               return;
                // console.log(position);
-               // debug("First: " + position.coords.latitude + ", " + position.coords.longitude);
-
-               let lat = position.coords.latitude;
-               let lon = position.coords.longitude;
+               debug("new position: " + position.coords.latitude + ", " + position.coords.longitude);
+               user.lat = position.coords.latitude;
+               user.lon = position.coords.longitude;
+               background(user, flag);
              });
 
-             //navigator.geolocation.watchPosition(function(position) {
+             navigator.geolocation.watchPosition(function(position) {
                // console.log(position);
-             //  debug(position.coords.latitude + ", " + position.coords.longitude);
-             //});
+               debug("new position: " + position.coords.latitude + ", " + position.coords.longitude);
+               user.lat = position.coords.latitude;
+               user.lon = position.coords.longitude;
+               background(user, flag);
+             });
 
+             // document.body.webkitRequestFullscreen();
              window.addEventListener("deviceorientation", (e) => {
+               debug("new orientation");
+               user.heading = e.beta;
+               // return;
+               // background(user, flag);
+               
                // debug(e.gamma * 4);
                document.getElementById("arrow").style.transform = 
                "rotateX(" + (180 + e.gamma * 4) + "deg)" +
-               "translateY(-1em)" +
+               "translateY(-4em)" +
+               "translateX(-8em)" +
                "rotateY(" + (- e.beta * 4) + "deg)";
           
                //"rotateZ(" + e.alpha + "deg)" +
