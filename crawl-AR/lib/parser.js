5997d761f2f8d9776082472f8d504689cd27aa70
Michal Mocny <mmocny@gmail.com>
2018-06-29 12:09:14 -0400

    Support dataFeedElement and nested DataFeeds

--- crawl-AR/lib/parser.js
+++ crawl-AR/lib/parser.js
@@ -3,55 +3,75 @@ const EventEmitter = require('events');
 /******************************************************************************/
 class ParserError extends Error {};
 
+/*
+ * Parser accepts a jsonld block and will walk its tree emitting events
+ * whenever if finds a new DataFeed, ArArtifact, or certain types of Anchors
+ *
+ * Each event will include references to the root jsonld original provided,
+ * as well as the enclosing DataFeed or ArArtifact if applicable.
+ *
+ * TODO: parsing is done using simple JSON lookup and not using a JSON-LD library
+ * as I was unable to get the `jsonld` library working well (install time compile issues).
+ * As such, parsing is not enforcing correct formatting, does not support case insensitivity,
+ * does not do even simple transformations etc.  Only perfectly formed inputs work.
+ *
+ * TODO: Events refer only back to a single root artifact or datafeed.  While the parser
+ * does support nested DataFeeds, you will not know about grandparent DataFeeds from
+ * fired events.
+ *
+ * */
 class Parser extends EventEmitter {
   parse(jsonld) {
+    this._parseUnknown(jsonld, { root: jsonld });
+  }
+
+  _parseUnknown(jsonld, params) {
     if (!('@type' in jsonld)) {
-      throw new ParserError("Json-ld does not contain @type");
+      console.warn("Json-ld does not contain @type");
+      return;
     }
 
     let type = jsonld['@type']
 
     if (typeof type !== 'string') {
-      throw new ParserError("Json-ld @type is not string");
+      console.warn("Json-ld @type is not string");
+      return;
     }
 
     type = type.toLowerCase().trim();
 
     switch (type) {
       case "datafeed":
-        return this._parseDataFeed({
-          root: jsonld,
-          feed: jsonld,
-        });
+        this._parseDataFeed(Object.assign(params, { datafeed: jsonld }));
+        break;
 
       case "arartifact":
-        return this._parseArArtifact({
-          root: jsonld,
-          artifact: jsonld,
-        });
+        this._parseArArtifact(Object.assign(params, { arartifact: jsonld }));
+        break;
 
       default:
-        throw new Error("Json-ld does not contain DataFeed or ARArtifact");
+        break; // We ignore syntax we don't understand, and move on
     }
   }
 
   _parseDataFeed(params) {
-    let { root, feed } = params;
+    let { root, datafeed } = params;
 
     this.emit('data-feed', Object.assign({}, params));
 
-    // TODO: iterate over datafeed items, call _parseArArtifact
-
+    for (let dataFeedElement of (datafeed["dataFeedElement"] || [])) {
+      this._parseUnknown(dataFeedElement, params);
+    }
   }
 
   _parseArArtifact(params) {
-    let { root, feed, artifact } = params;
+    let { root, datafeed, arartifact } = params;
 
     this.emit('ar-artifact', params);
   }
 
   _parseLandmark(params) {
-    let { root, feed, artifact, landmark } = params;
+    let { root, datafeed, arartifact, landmark } = params;
 
     // http://schema.org/GeoCoordinates
     let { address, addressCountry, elevation, latitude, longitude, postalCode } = nearby;
