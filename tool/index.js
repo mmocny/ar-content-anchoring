d35c17fb7eecdc517e44598371ca48b14e6abd7e
Michal Mocny <mmocny@gmail.com>
2018-06-29 13:56:43 -0400

    adding simple CLI parser

--- tool/index.js
+++ tool/index.js
@@ -7,7 +7,36 @@ global.__basedir = __dirname;
 /******************************************************************************/
 
 async function main() {
-  await require('./sample').sample()
+  const parseArgs = require('minimist');
+  const opts = {
+    string: [], // argument names to always treat as strings
+    boolean: [], // argument names to always treat as booleans.  Double hyphen: --Arg means true
+    alias: {
+    }, // maps argument to list of aliases
+    default: {
+    }, // maps argument names to default values
+
+    stopEarly: false, // when true, populate everything after first non-arg as `_` values
+    '--': true, // when true, `_` includes only up to `--` and `--` includes the rest
+    /* // Unknown seems to be called for all options..
+    unknown: (arg) => {
+      console.warn("Unexpected argument:", arg);
+    },
+    */
+  };
+
+  let argv = parseArgs(process.argv.slice(2), opts);
+
+  let command = (argv._ || ["sample"]) [0]
+  switch (command) {
+    case "sample":
+      await require('./sample').sample();
+      break;
+
+    default:
+      console.warn("Unknown command:", command);
+      break;
+  }
 }
 
 /******************************************************************************/
