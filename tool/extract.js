aca95e7998c249f85a554f7304a7437d7492c47e
Michal Mocny <mmocny@gmail.com>
2018-06-29 14:17:05 -0400

    Create CLI webar-ca; create extract function

---
+++ tool/extract.js
@@ -0,0 +1,41 @@
+/******************************************************************************/
+
+async function extract(urls) {
+  /* What are we going to index? */
+  let sites = [].concat(urls); // if urls is just one url, concat converts to array
+
+  /* Lets fetch (or open) the HTML and extract JSON-LD */
+  let crawler = require('./lib/crawler');
+  let crawl_results = await crawler.crawl(sites);
+
+  /* Set up a simple ArArtifacts JSON-LD parser */
+  const Parser = require('./lib/parser');
+  let parser = new Parser();
+
+  /* Listen to parser events, update index as needed */
+  parser.on('data-feed', async(params) => {
+    console.log(JSON.stringify(params.datafeed, function (k, v) { return k ? "" + v : v; }, 2));
+  });
+  parser.on('ar-artifact', async(params) => {
+    console.log(JSON.stringify(params.arartifact, function (k, v) { return k ? "" + v : v; }, 2));
+  });
+  parser.on('anchor-address', async(params) => {
+    console.log(JSON.stringify(params, function (k, v) { return k ? "" + v : v; }));
+  });
+  parser.on('anchor-geolocation', async(params) => {
+    console.log(JSON.stringify(params, function (k, v) { return k ? "" + v : v; }));
+  });
+
+  /* Now ask parser to parse all the crawled jsonld */
+  for (crawl_result of crawl_results) {
+    try {
+      parser.parse(crawl_result.jsonld);
+    } catch(ex) {
+      console.error("Parser Error:", ex.message);
+    };
+  }
+}
+
+/******************************************************************************/
+
+module.exports.extract = extract;
--- tool/index.js
+++ tool/index.js
@@ -1,4 +1,4 @@
-/* Hello! */
+#! /usr/bin/env node
 
 /******************************************************************************/
 
@@ -6,12 +6,29 @@ global.__basedir = __dirname;
 
 /******************************************************************************/
 
+function help(argv) {
+  console.log("Help yourself");
+}
+
+/******************************************************************************/
+
+function unknown(argv) {
+  console.log(`Valid commands:
+    help
+    sample
+    extract
+`);
+}
+
+/******************************************************************************/
+
 async function main() {
   const parseArgs = require('minimist');
   const opts = {
     string: [], // argument names to always treat as strings
-    boolean: [], // argument names to always treat as booleans.  Double hyphen: --Arg means true
+    boolean: [ "help" ], // argument names to always treat as booleans.  Double hyphen: --Arg means true
     alias: {
+      "help": "-h",
     }, // maps argument to list of aliases
     default: {
     }, // maps argument names to default values
@@ -27,14 +44,26 @@ async function main() {
 
   let argv = parseArgs(process.argv.slice(2), opts);
 
-  let command = (argv._ || ["sample"]) [0]
+  if (argv.h) {
+    return help();
+  }
+
+  let command = argv._[0];
   switch (command) {
+    case "extract":
+      await require("./extract").extract(argv._.slice(1));
+      break;
+
+    case "help":
+      help(argv);
+      break;
+
     case "sample":
       await require('./sample').sample();
       break;
 
     default:
-      console.warn("Unknown command:", command);
+      unknown(argv);
       break;
   }
 }
